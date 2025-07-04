63739bbbcb36d5d51cc37126391d91a2d1b4f6b5
NeroGeer <159979503+NeroGeer@users.noreply.github.com>
2025-07-04 13:19:59 +1000

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ from sqlalchemy.future import select
 import models as models
 import schemas as schemas
 from database import SessionDep, engine
-
-
+a
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Код при старте приложения (startup)
